b3e8b9c8e5cffbcfac77df05c4ed78b908cb2480
Araden14 <arnaud-danselme@hotmail.com>
2025-03-19 10:08:36 +0000

    [FEAT] Tests fonctionnels du TaskManager

--- tests/TaskManagerTest.php
+++ tests/TaskManagerTest.php
@@ -7,10 +7,38 @@ use PHPUnit\Framework\TestCase;
 
 class TaskManagerTest extends TestCase {
 
-    public function testTaskManager(){
+    public function testAddTask(){
         $taskmanager = new TaskManager();
         $task = "Task 1";
         $taskmanager->addTask($task);
-        $this->assertEquals([$task], $taskmanager->getTasks());
+        $this->assertCount(1, $taskmanager->getTasks());
     }
+    // Scénario : On crée une tâche, on la supprime et 
+    // on vérifie que l'exception OutofBounds fonctionne pour getTask() et removeTask()
+    public function testRemoveTask(){
+        $taskmanager = new TaskManager();
+        $task = "Task 1";
+        $taskmanager->addTask($task);
+        $taskmanager->removeTask(0);
+        $this->expectException(\OutOfBoundsException::class);
+        $taskmanager->removeTask(1);
+        $this->expectException(\OutOfBoundsException::class);
+        $taskmanager->getTask(0);
+        $this->assertCount(0, $taskmanager->getTasks());
+    }
+    // On vérifie si la logique d'attribution d'indice 
+    // fonctionne correctement
+    public function testTaskOrderAfterRemoval(){
+        $taskmanager = new TaskManager();
+        $task1 = "Task 1";
+        $task2 = "Task 2";
+        $taskmanager->addTask($task1);
+        $taskmanager->addTask($task2);
+        $this->assertEquals("Task 1", $taskmanager->getTask(0));
+        $this->assertEquals("Task 2", $taskmanager->getTask(1));
+        $taskmanager->removeTask(0);
+        $this->assertEquals("Task 2", $taskmanager->getTask(0));
+
+    }
+    
 }
